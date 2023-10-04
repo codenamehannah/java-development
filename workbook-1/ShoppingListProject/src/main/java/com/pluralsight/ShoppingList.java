d8a5c72f4328109f205e39f85c7e16f125f423f8
codenamehannah <jhannahstuart@gmail.com>
2023-10-04 09:48:25 -0400

    completed ShoppingList app

---
+++ workbook-1/ShoppingListProject/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Go to Publix and grab Ginger, Curry Powder, Carrots, Jasmine Rice, Coconut Milk, Thyme, Scotch Bonnet, Onions, Potatoes and Garlic");
+    }
+}
