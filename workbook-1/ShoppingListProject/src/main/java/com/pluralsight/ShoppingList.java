16fa625ca8e5bfe0ecd47876d2b69f77eaf0f3cc
codenamehannah <jhannahstuart@gmail.com>
2023-10-04 10:26:45 -0400

    Completed ShoppingList app: Edited because i put it all on one row.

--- workbook-1/ShoppingListProject/src/main/java/com/pluralsight/ShoppingList.java
+++ workbook-1/ShoppingListProject/src/main/java/com/pluralsight/ShoppingList.java
@@ -2,6 +2,15 @@ package com.pluralsight;
 
 public class ShoppingList {
     public static void main(String[] args) {
-        System.out.println("Go to Publix and grab Ginger, Curry Powder, Carrots, Jasmine Rice, Coconut Milk, Thyme, Scotch Bonnet, Onions, Potatoes and Garlic");
+        System.out.println("Ginger");
+        System.out.println("Onion");
+        System.out.println("Curry Powder");
+        System.out.println("Carrots");
+        System.out.println("Jasmine Rice");
+        System.out.println("Coconut Milk");
+        System.out.println("Thyme");
+        System.out.println("Scotch Bonnet");
+        System.out.println("Potatoes");
+        System.out.println("Garlic");
     }
 }
