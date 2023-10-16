c4cad14976beb79c56128d092123c5ce9fc0b77f
codenamehannah <jhannahstuart@gmail.com>
2023-10-16 18:48:09 -0400

    Completed the neighborhood library.

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/FileReader.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class FileReader {
+    public static void main(String[] args) {
+        int x = 9;
+        if (x++ < 10) {
+            System.out.println("Low");
+        } else {
+            System.out.println("High");
+
+        }
+    }
+
+}
+
+
--- workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -5,28 +5,7 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        //First Create a new File input Stream
-        //Instead of putting new scanner or System.in, you put the name of the file.
-        // Specify the file we're using.
-        //use a while loop (scanner.hasNextLine)
-        try {
-            FileInputStream fis = new FileInputStream("poem.txt");
 
-            Scanner scanner = new Scanner(fis);
-
-            String input;
-            //to read code line by line in the file.
-
-            while(scanner.hasNextLine()){
-                input = scanner.nextLine();
-                System.out.println(input);
-            }
-
-            //use the scanner closer always!
-            scanner.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-
-        }
     }
 }
+
