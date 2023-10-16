1bd1c17c1941f93e0075d04b548f784de33a250c
codenamehannah <jhannahstuart@gmail.com>
2023-10-16 13:57:03 -0400

    Completed the bedtime stories exercise but incomplete with an error.

--- workbook-2/BedtimeStories/src/main/java/com/pluralsight/BedtimeStories.java
+++ workbook-2/BedtimeStories/src/main/java/com/pluralsight/BedtimeStories.java
@@ -3,30 +3,31 @@ package com.pluralsight;
 import java.io.FileInputStream;
 import java.util.Scanner;
 
-public class HelloWorld {
+public class BedtimeStories {
+    public BedtimeStories() {
+    }
+
     public static void main(String[] args) {
-        //First Create a new File input Stream
-        //Instead of putting new scanner or System.in, you put the name of the file.
-        // Specify the file we're using.
-        //use a while loop (scanner.hasNextLine)
         try {
-            FileInputStream fis = new FileInputStream("poem.txt");
 
+            FileInputStream fis = new FileInputStream("goldilocks.txt");
             Scanner scanner = new Scanner(fis);
-
             String input;
+
             //to read code line by line in the file.
 
-            while(scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 input = scanner.nextLine();
                 System.out.println(input);
-            }
 
+            }
             //use the scanner closer always!
             scanner.close();
+
         } catch (Exception ex) {
             ex.printStackTrace();
 
         }
     }
 }
+
